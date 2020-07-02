1d58949f2695f19eb2aaa085597321021a12bcce
Are_Alb_Martins <arethalbuquerque@gmail.com>
2020-07-02 19:41:06 -0300

    Adição funções de alteração e exclusão de dados

--- agendadjango/urls.py
+++ agendadjango/urls.py
@@ -25,10 +25,9 @@ urlpatterns = [
     path('agenda/',views.lista_eventos),
     path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
+    path('agenda/evento/delete/<int:id_evento>/', views.deletevento),
     path ('', RedirectView.as_view(url='/agenda/')),
     path ('login/', views.login_user),
     path ('login/submit', views.submit_login),
     path ('logout/', views.logout_user)
-
-
 ]
